602bfa9bc38e53bb736ae44592d8d54b15c42c23
Enrique Rivera <erivera7240@gmail.com>
2023-10-14 23:36:12 -0500

    need to collect data but this is template code

--- index.py
+++ index.py
@@ -9,3 +9,25 @@ from datetime import datetime
 
 import warnings
 warnings.filterwarnings("ignore")
+
+data = pd.read_csv('./s_p_stock/all_stocks_5yr.csv')
+print(data.shape)
+print(data.sample(7))
+
+data.info()
+
+data['date'] = pd.to_datetime(data['date'])
+data.info()
+
+data['date'] = pd.to_datetime(data['date'])
+# date vs open
+# date vs close
+plt.figure(figsize=(15, 8))
+for index, company in enumerate(companies, 1):
+	plt.subplot(3, 3, index)
+	c = data[data['Name'] == company]
+	plt.plot(c['date'], c['close'], c="r", label="close", marker="+")
+	plt.plot(c['date'], c['open'], c="g", label="open", marker="^")
+	plt.title(company)
+	plt.legend()
+	plt.tight_layout()
